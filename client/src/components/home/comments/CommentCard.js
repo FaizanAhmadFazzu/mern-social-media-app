209080be045f90f6ce1b021105630198da578f7c
Faizan Ahmad Khan <ahmadfaizan1604@gmail.com>
2021-11-12 17:05:52 +0530

    Edit Like Comment

--- client/src/components/home/comments/CommentCard.js
+++ client/src/components/home/comments/CommentCard.js
@@ -1,25 +1,56 @@
 import React, { useState, useEffect } from "react";
 import Avatar from "../../Avatar";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import moment from "moment";
 
 import LikeButton from "../../LikeButton";
 import CommentMenu from "./CommentMenu";
+
+import { updateComment, likeComment, unLikeComment } from "../../../redux/actions/commentAction"
 const CommentCard = ({ comment, post }) => {
+
+  const dispatch = useDispatch();
   const [content, setContent] = useState("");
   const [readMore, setReadMore] = useState(false);
   const [isLike, setIsLike] = useState(false);
+  const [loadLike, setLoadLike] = useState(false);
+  const [onEdit, setOnEdit] = useState(false);
 
   const { theme, auth } = useSelector((state) => state);
 
   useEffect(() => {
     setContent(comment.content);
-  }, [comment]);
+    setIsLike(false);
+    if(comment.likes.find(like => like._id === auth.user._id)){
+      setIsLike(true);
+    }
+  }, [comment, auth.user._id]);
 
-  const handleLike = () => {};
+  const handleUpdate = () => {
+    if(comment.content !== content) {
+      dispatch(updateComment({ comment, post, content, auth }))
+      setOnEdit(false);
+    } else {
+      setOnEdit(false);
+    }
+  }
 
-  const handleUnLike = () => {};
+  const handleLike = async () => {
+    if (loadLike) return;
+    setIsLike(true);
+    setLoadLike(true);
+    await dispatch(likeComment({ comment, post, auth }))
+    setLoadLike(false);
+  };
+
+  const handleUnLike = async () => {
+    if (loadLike) return;
+    setIsLike(false);
+    setLoadLike(true);
+    await dispatch(unLikeComment({ comment, post, auth }))
+    setLoadLike(false);
+  };
   const styleCard = {
     opacity: comment._id ? 1 : 0.5,
     pointerEvents: comment._id ? "inherit" : "none",
@@ -38,21 +69,32 @@ const CommentCard = ({ comment, post }) => {
             color: theme ? "white" : "#111",
           }}
         >
-          <div>
-            <span>
-              {content.length < 100
-                ? content
-                : readMore
-                ? content + " "
-                : content.slice(0, 100) + "....."}
-            </span>
-
-            {content.length > 100 && (
-              <span className="readMore" onClick={() => setReadMore(!readMore)}>
-                {readMore ? "Hide Content" : "Read More"}
+          {onEdit ? (
+            <textarea
+              rows="5"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+          ) : (
+            <div>
+              <span>
+                {content.length < 100
+                  ? content
+                  : readMore
+                  ? content + " "
+                  : content.slice(0, 100) + "....."}
               </span>
-            )}
-          </div>
+
+              {content.length > 100 && (
+                <span
+                  className="readMore"
+                  onClick={() => setReadMore(!readMore)}
+                >
+                  {readMore ? "Hide Content" : "Read More"}
+                </span>
+              )}
+            </div>
+          )}
 
           <div style={{ cursor: "pointer" }}>
             <small className="text-muted mr-3">
@@ -61,8 +103,19 @@ const CommentCard = ({ comment, post }) => {
             <small className="font-weight-bold mr-3">
               {comment.likes.length} likes
             </small>
-
-            <small className="font-weight-bold mr-3">reply</small>
+            {onEdit ? (
+              <>
+                <small className="font-weight-bold mr-3" onClick={handleUpdate}>update</small>
+                <small
+                  className="font-weight-bold mr-3"
+                  onClick={() => setOnEdit(false)}
+                >
+                  cancel
+                </small>
+              </>
+            ) : (
+              <small className="font-weight-bold mr-3">reply</small>
+            )}
           </div>
         </div>
 
@@ -70,7 +123,12 @@ const CommentCard = ({ comment, post }) => {
           className="d-flex align-items-center mx-2"
           style={{ cursor: "pointer" }}
         >
-          <CommentMenu post={post} comment={comment} auth={auth} />
+          <CommentMenu
+            post={post}
+            comment={comment}
+            auth={auth}
+            setOnEdit={setOnEdit}
+          />
           <LikeButton
             isLike={isLike}
             handleLike={handleLike}
--- client/src/redux/actions/commentAction.js
+++ client/src/redux/actions/commentAction.js
@@ -1,5 +1,5 @@
-import { postDataAPI } from "../../utils/fetchData";
-import { GLOBALTYPES } from "./globalTypes";
+import { patchDataAPI, postDataAPI } from "../../utils/fetchData";
+import { DeleteData, EditData, GLOBALTYPES } from "./globalTypes";
 import { POST_TYPES } from "./postAction";
 
 export const createComment =
@@ -8,11 +8,65 @@ export const createComment =
     const newPost = { ...post, comments: [...post.comments, newComment] };
     dispatch({ type: POST_TYPES.UPDATE_POST, payload: newPost });
     try {
-        const data = { ...newComment, postId: post._id };
-        const res = await postDataAPI("comment", data, auth.token)
-        const newData = { ...res.data.newComment, user: auth.user }
-        const newPost = { ...post, comments: [...post.comments, newData] };
-        dispatch({ type: POST_TYPES.UPDATE_POST, payload: newPost });
+      const data = { ...newComment, postId: post._id };
+      const res = await postDataAPI("comment", data, auth.token);
+      const newData = { ...res.data.newComment, user: auth.user };
+      const newPost = { ...post, comments: [...post.comments, newData] };
+      dispatch({ type: POST_TYPES.UPDATE_POST, payload: newPost });
+    } catch (err) {
+      dispatch({
+        type: GLOBALTYPES.ALERT,
+        payload: { error: err.response.data.msg },
+      });
+    }
+  };
+
+export const updateComment =
+  ({ comment, post, content, auth }) =>
+  async (dispatch) => {
+    const newComments = EditData(
+      post.comments,
+      { ...comment, content },
+      comment._id
+    );
+    const newPost = { ...post, comments: newComments };
+    dispatch({ type: POST_TYPES.UPDATE_POST, payload: newPost });
+    try {
+      await patchDataAPI(`comment/${comment._id}`, { content }, auth.token);
+    } catch (err) {
+      dispatch({
+        type: GLOBALTYPES.ALERT,
+        payload: { error: err.response.data.msg },
+      });
+    }
+  };
+
+export const likeComment =
+  ({ comment, post, auth }) =>
+  async (dispatch) => {
+    const newComment = { ...comment, likes: [...comment.likes, auth.user] };
+    const newComments = EditData(post.comments, newComment, comment._id);
+    const newPost = { ...post, comments: newComments };
+    dispatch({ type: POST_TYPES.UPDATE_POST, payload: newPost });
+    try {
+      await patchDataAPI(`comment/${comment._id}/like`, null, auth.token);
+    } catch (err) {
+      dispatch({
+        type: GLOBALTYPES.ALERT,
+        payload: { error: err.response.data.msg },
+      });
+    }
+  };
+
+export const unLikeComment =
+  ({ comment, post, auth }) =>
+  async (dispatch) => {
+    const newComment = { ...comment, likes: DeleteData(comment.likes, auth.user._id) };
+    const newComments = EditData(post.comments, newComment, comment._id);
+    const newPost = { ...post, comments: newComments };
+    dispatch({ type: POST_TYPES.UPDATE_POST, payload: newPost });
+    try {
+      await patchDataAPI(`comment/${comment._id}/unlike`, null, auth.token);
     } catch (err) {
       dispatch({
         type: GLOBALTYPES.ALERT,
